4cb52643b448d2518fb78ed82b9ca9133bf204a3
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-06-06 23:30:20 -0300

     :package: feat(): Adicionando axios e toast notify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { createApp } from 'vue';
 import './style.css';
 import router from './route';
+
+// pages
 import Home from '@pages/Home/Home.vue';
 import Login from '@pages/Login/Login.vue';
 import Register from '@pages/Register/Register.vue';
 import App from './App.vue';
 
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
 const app = createApp(App);
+
+const toastOptions: ToastContainerOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+};
+
+app.use(Vue3Toastify, toastOptions);
 
 app.component('Home', Home);
 app.component('Login', Login);

ea48a82ceedbd9d83311ad26f451d49df7b84ab4
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-06-08 17:59:10 -0300

    :sparkles: feat(): Adicionando props para as cartas

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="w-[20rem] h-[26.25rem] border-2 border-darkGrey rounded-lg">
+  <div class="w-[20rem] h-[26.25rem] mt-[4rem] border-2 border-darkGrey rounded-lg">
     <div
       class="h-[21.25rem]  flex flex-col justify-center items-center"
     >
-      <img src="https://images.ygoprodeck.com/images/cards/44553392.jpg" class="h-[20rem]"/>
+      <img :src="imageUrl" class="h-[20rem]"/>
     </div>
     <div class="bg-darkGrey h-[5rem] rounded-b-lg flex items-center" >
-      <p class="text-white text-[1.1rem] ml-[1rem]" >Nome da carta</p>
+      <p class="text-white text-[1.1rem] ml-[1rem]" >{{ name }}</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { defineProps } from 'vue';
+  import { ModelCard } from '@models/Card';
+
+  const props = defineProps<Pick<ModelCard, 'name' | 'imageUrl'>>();
+
+  const { name, imageUrl } = props;
+
 </script>
 
 <style>

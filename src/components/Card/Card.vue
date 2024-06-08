f6d47b1344ac4afaf7d013b130489969661829af
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-06-08 15:18:44 -0300

    :sparkles: feat(): Adicionando card (carta cru)

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-[20rem] h-[26.25rem] border-2 border-darkGrey rounded-lg">
+    <div
+      class="h-[21.25rem]  flex flex-col justify-center items-center"
+    >
+      <img src="https://images.ygoprodeck.com/images/cards/44553392.jpg" class="h-[20rem]"/>
+    </div>
+    <div class="bg-darkGrey h-[5rem] rounded-b-lg flex items-center" >
+      <p class="text-white text-[1.1rem] ml-[1rem]" >Nome da carta</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>

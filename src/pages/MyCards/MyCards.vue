10dd685e32ce10af9c92cf756e7fc7073007bf0d
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-06-08 18:24:54 -0300

    :sparkles: feat(): Adicionando integração para as minhas cartas

--- src/pages/MyCards/MyCards.vue
+++ src/pages/MyCards/MyCards.vue
@@ -4,11 +4,17 @@
     <div class="w-[100%] h-full flex flex-row">
       <Menu v-if="!isTabletOrMobile" />
       <div class="p-[3rem] w-[100%]">
-        <div class="w-[100%] flex flex-row justify-between items-center">
-          <p class="text-[2.5rem] font-bold text-darkGrey" >Minhas Cartas</p>
+        <div class="w-[100%] flex flex-row items-center justify-between">
+          <div class="w-[50%] flex flex-row items-center">
+            <p class="text-[2.5rem] font-bold text-darkGrey">Minhas cartas</p>
+          </div>
         </div>
-        <div class="mt-[4rem]" >
-          <Card />
+        <div class="flex flex-row flex-wrap">
+          <template v-for="row in cards" :key="row.id">
+            <div class="flex flex-col justify-center items-center mr-[3rem]" v-if="row.name.length > 0">
+              <Card :name="row.name" :imageUrl="row.imageUrl" />
+            </div>
+          </template>
         </div>
       </div>
     </div>
@@ -16,13 +22,27 @@
 </template>
 
 <script setup lang="ts">
-  import { ref, inject } from "vue";
-
+  import { ref, inject, watchEffect } from "vue";
   import Header from '@components/Header/Header.vue'
   import Menu from '@components/Menu/Menu.vue'
   import Card from '@components/Card/Card.vue'
+  import { ModelCard } from "@models/Card";
+  import { CardService } from "@services/Card/userCard";
 
   const isTabletOrMobile = inject("isTabletOrMobile", ref(false));
+  const cards = ref<ModelCard[]>([]);
+
+  const getCards = async () => {
+    const result = await CardService.GetByUser();
+    if (result) {
+      cards.value = result;
+    }
+  }
+
+  watchEffect(() => {
+    getCards();
+  });
+
 </script>
 
 <style>

310614f349456d885e176f2d2d42a4b2c25668e6
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-06-08 18:25:25 -0300

    :sparkles: feat(): Adicionando service get cards by user

--- src/services/Card/userCard.ts
+++ src/services/Card/userCard.ts
@@ -1,7 +1,30 @@
+import { BuyCards } from "@models/Card";
 import baseUrlApi from "@utils/baseUrlApi";
 import axios, { AxiosResponse } from "axios";
 
 export const CardService = {
+  GetByUser: async () => {
+    try {
+      const API_URL = `${baseUrlApi.baseUrlApi}/me/cards`;
+      const token = localStorage.getItem("token");
+      const httpHeaders = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      const response: AxiosResponse = await axios.get(
+        API_URL,
+        {
+          headers: httpHeaders,
+        }
+      );
+
+      if (response) {
+        return response.data;
+      }
+    } catch (error) {
+      throw new Error("Não foi possível obter suas cartas");
+    }
+  },
   GetAll: async (rpp = 10, page = 1) => {
     try {
       const API_URL = `${baseUrlApi.baseUrlApi}/cards`;
@@ -23,6 +46,25 @@ export const CardService = {
       }
     } catch (error) {
       throw new Error("Não foi possível obter as cartas");
+    }
+  },
+  Post: async (cards: BuyCards) => {
+    try {
+      const API_URL = `${baseUrlApi.baseUrlApi}/me/cards`;
+      const token = localStorage.getItem("token");
+      const httpHeaders = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      const response: AxiosResponse = await axios.post(API_URL, cards, {
+        headers: httpHeaders,
+      });
+
+      if (response) {
+        return response.data;
+      }
+    } catch (error) {
+      throw new Error("Não foi possível adicionar cartas");
     }
   },
 };
